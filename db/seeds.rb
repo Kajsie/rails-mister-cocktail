42f2325512a135c7ddf9444c372d24e0a059e603
kajsa kristersson <k.kristersson@gmail.com>
2017-08-18 12:04:43 +0200

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "milk")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic water")
+Ingredient.create(name: "baileys")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "coca cola")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "aperol")
+Ingredient.create(name: "campari")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "basil")
+Ingredient.create(name: "pineapple")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "ice tea")
+Ingredient.create(name: "bourbon")
+Ingredient.create(name: "chocolate liquor")
+Ingredient.create(name: "lime")
+
 
 Cocktail.create(name: "Mojito")
 Cocktail.create(name: "Caipirinja")
